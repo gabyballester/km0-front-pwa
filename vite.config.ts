058bb20523f1d15ce2dceba3f36ff2f9511caa89
gabyballester <gaby.ballester@gmail.com>
2025-02-19 21:53:32 +0100

    Generate icons

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,28 @@ export default defineConfig({
         short_name: "Vite PWA Project",
         theme_color: "#ffffff",
         icons: [
-          // Configuración de íconos
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
         ],
       },
     }),
