091bb07398ce1409ebec7d7d590603b9a3b7b77e
gabyballester <gaby.ballester@gmail.com>
2025-06-21 17:07:20 +0200

    chore: bump version for master push

--- src/shared/utils/build-info.generated.ts
+++ src/shared/utils/build-info.generated.ts
@@ -7,17 +7,17 @@
 import type { VersionInfo } from '@constants';
 
 export const BUILD_INFO: VersionInfo = {
-  "versionNumber": 9,
-  "version": "v9",
+  "versionNumber": 10,
+  "version": "v10",
   "buildDate": "2025-06-21",
-  "buildTime": "17:01",
-  "timestamp": 1750518100638,
+  "buildTime": "17:07",
+  "timestamp": 1750518439836,
   "environment": "development"
 };
 
-export const VERSION_NUMBER = 9;
-export const VERSION_STRING = 'v9';
+export const VERSION_NUMBER = 10;
+export const VERSION_STRING = 'v10';
 export const BUILD_DATE = '2025-06-21';
-export const BUILD_TIME = '17:01';
-export const BUILD_TIMESTAMP = 1750518100638;
+export const BUILD_TIME = '17:07';
+export const BUILD_TIMESTAMP = 1750518439836;
 export const BUILD_ENVIRONMENT = 'development';
